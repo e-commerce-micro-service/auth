ba2afeab66ff1950de0b706023a5b1e9ae4a3613
gmonaco3 <giacomomonaco1993@gmail.com>
2021-06-09 16:34:10 +0200

    first commit

---
+++ src/test/java/it/prova/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
